fad2872b2bd190d0ab692da3fdb98408812af753
Danielle Jenkins <djenkins@splunk.com>
2015-07-08 10:59:37 -0700

    Add support for capturing junit xml.

--- splunkins/src/main/java/jenkins/plugins/splunkins/SplunkinsNotifier.java
+++ splunkins/src/main/java/jenkins/plugins/splunkins/SplunkinsNotifier.java
@@ -2,6 +2,7 @@ package jenkins.plugins.splunkins;
 
 import hudson.EnvVars;
 import hudson.Extension;
+import hudson.FilePath;
 import hudson.Launcher;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
@@ -23,12 +24,14 @@ import java.util.logging.Logger;
 public class SplunkinsNotifier extends Notifier {
     public int maxLines;
     public boolean failBuild;
+    public String junitReport;
     private final static Logger LOGGER = Logger.getLogger(SplunkinsNotifier.class.getName());
 
     @DataBoundConstructor
-    public SplunkinsNotifier(int maxLines, boolean failBuild){
+    public SplunkinsNotifier(int maxLines, boolean failBuild, String junitReport){
         this.maxLines = maxLines;
         this.failBuild = failBuild;
+        this.junitReport = junitReport;
     }
 
 
@@ -51,8 +54,28 @@ public class SplunkinsNotifier extends Notifier {
             e.printStackTrace();
         }
 
+
+        FilePath workspacePath = build.getWorkspace(); // collect junit xml file
+
         LOGGER.info(log.toString());
         LOGGER.info(envVars.toString());
+        LOGGER.info("junitReport Path:" + this.junitReport.toString());
+        LOGGER.info("workspace path:" + workspacePath);
+
+        if (!this.junitReport.equals("")){
+            FilePath fullReportPath = new FilePath(workspacePath, this.junitReport.toString());
+            String report = null;
+            try {
+                report = fullReportPath.readToString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            LOGGER.info("fullReportPath:" + fullReportPath);
+            LOGGER.info(report);
+        }
+
         return !(failBuild);
     }
 
