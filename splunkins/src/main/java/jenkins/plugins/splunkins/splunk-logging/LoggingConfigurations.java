8eb6b41c2ed18d1cf8e2e717265a347a3cc1ab24
Danielle Jenkins <djenkins@splunk.com>
2015-07-13 11:57:16 -0700

    Add Kashyap's contributions for Splunk instance communication.

---
+++ splunkins/src/main/java/jenkins/plugins/splunkins/splunk-logging/LoggingConfigurations.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.LogManager;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import com.splunk.ServiceArgs;
+import ch.qos.logback.core.joran.spi.JoranException;
+
+
+public class LoggingConfigurations {
+	
+	 /*
+    create logging.property and force java logging  manager to reload the configurations
+    */
+    public static void loadJavaLoggingConfiguration(String configFileTemplate, String configFile, HashMap<String, String> userInputs) throws IOException, JoranException {
+        ServiceArgs serviceArgs = SplunkConnector.getSplunkHostInfo();
+
+        String configFilePath = updateConfigFile(configFileTemplate, configFile, userInputs, serviceArgs);
+        FileInputStream configFileStream = new FileInputStream(configFilePath);
+        LogManager.getLogManager().readConfiguration(configFileStream);
+        configFileStream.close();
+    }
+    
+    /**
+     * modify the config file with the generated token, and configured splunk host,
+     * read the template from configFileTemplate, and create the updated configfile to configFile
+     */
+    public static String updateConfigFile(String configFileTemplate, String configFile, HashMap<String, String> userInputs, ServiceArgs serviceArgs) throws IOException {
+        SplunkConnector.getSplunkHostInfo();
+
+        String configFileDir = SplunkConnector.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        List<String> lines = Files.readAllLines(new File(configFileDir, configFileTemplate).toPath(), Charset.defaultCharset());
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).contains("%host%")) {
+                lines.set(i, lines.get(i).replace("%host%", serviceArgs.host));
+            }
+            if (lines.get(i).contains("%port%")) {
+                lines.set(i, lines.get(i).replace("%port%", serviceArgs.port.toString()));
+            }
+
+            if (lines.get(i).contains("%scheme%")) {
+                lines.set(i, lines.get(i).replace("%scheme%", serviceArgs.scheme));
+            }
+
+            String match = FindUserInputConfiguration(lines.get(i));
+            if (!match.isEmpty()) {
+                if (userInputs.keySet().contains(match))
+                    lines.set(i, lines.get(i).replace("%" + match + "%", userInputs.get(match)));
+                else
+                    lines.set(i, "");
+            }
+        }
+
+        String configFilePath = new File(configFileDir, configFile).getPath();
+        FileWriter fw = new FileWriter(configFilePath);
+        for (String line : lines) {
+            if (!line.isEmpty()) {
+                fw.write(line);
+                fw.write(System.getProperty("line.separator"));
+            }
+        }
+
+        fw.flush();
+        fw.close();
+
+        return configFilePath;
+    }
+    
+    private static String FindUserInputConfiguration(String line) {
+        String pattern = "%.*%";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(line);
+        if (m.find()) {
+            return m.group(0).substring(1, m.group(0).length() - 1);
+        } else
+            return "";
+    }
+
+}
---
+++ splunkins/src/main/java/jenkins/plugins/splunkins/splunk-logging/SplunkConnector.java
@@ -0,0 +1,159 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.splunk.HttpService;
+import com.splunk.ResponseMessage;
+import com.splunk.SSLSecurityProtocol;
+import com.splunk.Service;
+import com.splunk.ServiceArgs;
+
+public class SplunkConnector {
+	private static Service service;
+	private static final ServiceArgs serviceArgs = new ServiceArgs();
+    private static final String httpInputTokenEndpointPath = "/services/data/inputs/http";
+
+	public static String createHttpinput(String httpinputName) throws Exception {
+		connectToSplunk();
+
+		// enable logging endpoint
+		SplunkConnector.enableHttpinput();
+
+		// create a httpinput
+		Map args = new HashMap();
+		args.put("name", httpinputName);
+		args.put("description", "test http input");
+
+		deleteHttpinput(httpinputName);
+
+		ResponseMessage msg = service.post(httpInputTokenEndpointPath, args);
+		assert msg.getStatus() == 201;
+
+		// get httpinput token
+		args = new HashMap();
+		ResponseMessage response = service.get(httpInputTokenEndpointPath + "/"
+				+ httpinputName, args);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				response.getContent(), "UTF-8"));
+		String token = "";
+		while (true) {
+			String line = reader.readLine();
+			if (line == null)
+				break;
+
+			if (line.contains("name=\"token\"")) {
+				token = line.split(">")[1];
+				token = token.split("<")[0];
+				break;
+			}
+		}
+		reader.close();
+
+		if (token.isEmpty()) {
+			System.out.println("No ");
+		}
+
+		return token;
+	}
+
+	public static Service connectToSplunk() throws IOException {
+
+		if (service == null) {
+			getSplunkHostInfo();
+
+			// get splunk service and login
+			service = Service.connect(serviceArgs);
+			service.login();
+		}
+
+		return service;
+	}
+
+	/**
+	 * read splunk host info from .splunkrc file
+	 */
+	static ServiceArgs getSplunkHostInfo() throws IOException {
+
+		if (serviceArgs.isEmpty()) {
+			// set default value
+			serviceArgs.setUsername("admin");
+			serviceArgs.setPassword("changeme");
+			serviceArgs.setHost("localhost");
+			serviceArgs.setPort(8089);
+			serviceArgs.setScheme("https");
+
+			// update serviceArgs with splunk host info
+			String splunkhostfile = System.getProperty("user.home")
+					+ File.separator + ".splunkrc";
+			HttpService.setSslSecurityProtocol(SSLSecurityProtocol.TLSv1_2);
+
+			List<String> lines = Files
+					.readAllLines(new File(splunkhostfile).toPath(),
+							Charset.defaultCharset());
+			for (String line : lines) {
+				if (line.toLowerCase().contains("host=")) {
+					serviceArgs.setHost(line.split("=")[1]);
+				}
+				if (line.toLowerCase().contains("admin=")) {
+					serviceArgs.setUsername(line.split("=")[1]);
+				}
+				if (line.toLowerCase().contains("password=")) {
+					serviceArgs.setPassword(line.split("=")[1]);
+				}
+				if (line.toLowerCase().contains("scheme=")) {
+					serviceArgs.setScheme(line.split("=")[1]);
+				}
+				if (line.toLowerCase().contains("port=")) {
+					serviceArgs.setPort(Integer.parseInt(line.split("=")[1]));
+				}
+			}
+		}
+		return serviceArgs;
+	}
+
+	/**
+	 * enable http input features
+	 */
+	public static void enableHttpinput() throws IOException {
+		connectToSplunk();
+
+		// enable logging endpoint
+		Map args = new HashMap();
+		args.put("disabled", 0);
+		ResponseMessage response = service.post(
+				"/servicesNS/admin/search/data/inputs/http/http", args);
+		assert response.getStatus() == 200;
+
+		args.clear();
+		args.put("index", "main");
+		ResponseMessage index_response = service.post(
+				"/servicesNS/admin/search/data/inputs/http/http", args);
+		assert index_response.getStatus() == 200;
+
+	}
+
+	/**
+	 * delete http input token
+	 */
+	public static void deleteHttpinput(String httpinputName) throws Exception {
+		connectToSplunk();
+		try {
+			ResponseMessage response = service.get(httpInputTokenEndpointPath
+					+ "/" + httpinputName);
+			if (response.getStatus() == 200) {
+				response = service.delete(httpInputTokenEndpointPath + "/"
+						+ httpinputName);
+				assert response.getStatus() == 200;
+			}
+		} catch (com.splunk.HttpException e) {
+			if (e.getStatus() != 404)
+				throw e;
+		}
+	}
+
+}
