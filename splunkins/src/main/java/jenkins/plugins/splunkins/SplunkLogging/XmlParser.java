faa50cb1df4214ac98f3a3109caea2fd800d59cd
Kashyap Jotwani <kjotwani@splunk.com>
2015-07-20 15:48:31 -0700

    Fixed the parser bug

--- splunkins/src/main/java/jenkins/plugins/splunkins/SplunkLogging/XmlParser.java
+++ splunkins/src/main/java/jenkins/plugins/splunkins/SplunkLogging/XmlParser.java
@@ -1,5 +1,6 @@
 package jenkins.plugins.splunkins.SplunkLogging;
 
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -19,86 +20,131 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.logging.Logger;
 
 public class XmlParser {
-    private ArrayList<JSONObject> jsonObjects = new ArrayList<JSONObject>();
+	private ArrayList<JSONObject> jsonObjects = new ArrayList<JSONObject>();
+	private JSONObject finalJSON = new JSONObject();
+	private static boolean entryOnce;
+	
+	
+	public ArrayList<JSONObject> xmlParser(String logs, EnvVars envVars) {
+		Object xmlJSONObj = null;
+		ArrayList<JSONObject> jsonObjs = null;
 
-    public ArrayList<JSONObject> xmlParser(String logs, EnvVars envVars) {
-        Object xmlJSONObj = null;
-        ArrayList<JSONObject> jsonObjs = null;
+		try {
+			if (validateXMLSchema(Constants.xsdPath, logs)) {
+				xmlJSONObj = (JSONObject) XML.toJSONObject(logs);
+			} else {
+				// TODO
+			}
+			if (xmlJSONObj instanceof JSONObject) {
+				jsonObjs = parse((JSONObject) xmlJSONObj);
+				return jsonObjs;
 
-        try {
-                if (validateXMLSchema(Constants.xsdPath, logs)){
-                    xmlJSONObj = (JSONObject) XML.toJSONObject(logs);
-                }
-                else {
-                	//TODO
-                }
-                if (xmlJSONObj instanceof JSONObject) {
-                    jsonObjs = parse((JSONObject) xmlJSONObj);
-            		return jsonObjs;
-
-                } 
-        } catch (JSONException | ParseException e) {
-            e.printStackTrace();
-        }
+			}
+		} catch (JSONException | ParseException e) {
+			e.printStackTrace();
+		}
 		return jsonObjs;
-    }
+	}
 
-    private ArrayList<JSONObject> parse(JSONObject json) throws JSONException,
-            ParseException {
+	private ArrayList<JSONObject> parse(JSONObject json) throws JSONException,
+			ParseException {
 
-        Iterator<String> keys = json.keys();
-        JSONObject commonElements = new JSONObject();
+		if (!entryOnce) {
+			JSONObject transformedJSON = null;
+			Iterator<String> originalKeys = json.keys();
+			while (originalKeys.hasNext()) {
+				String key = originalKeys.next();
+				transformedJSON = customJSONObject(json.getJSONObject(key
+						.toString()));
+				entryOnce = true;
+			}
 
-        while (keys.hasNext()) {
-            String key = keys.next();
-            try {
-                JSONObject originalJSON = json.getJSONObject(key);
-                parse(originalJSON);
-                commonElements = originalJSON;
-            } catch (JSONException e) {
-                if (Constants.TESTCASE.equalsIgnoreCase(key)) {
-                    JSONArray jsonarr = json.getJSONArray(key);
-                    for (int n = 0; n < jsonarr.length(); n++) {
-                        JSONObject object = jsonarr.getJSONObject(n);
+			finalJSON.put(Constants.TESTSUITE, transformedJSON);
+		}
 
-                        JSONObject jsonFinal = new JSONObject();
-                        jsonFinal.put(key, object);
+		Iterator<String> keys = finalJSON.keys();
+		JSONObject commonElements = new JSONObject();
 
-                        jsonObjects.add(jsonFinal);
-                    }
-                }
-            }
-        }
-        commonElements.remove(Constants.TESTCASE);
-        return merge(commonElements, jsonObjects);
-    }
+		while (keys.hasNext()) {
+			String key = keys.next();
+			try {
+				JSONObject originalJSON = finalJSON.getJSONObject(key);
+				finalJSON = originalJSON;
+				parse(finalJSON);
+				commonElements = originalJSON;
+			} catch (JSONException e) {
+				if (Constants.TESTCASE.equalsIgnoreCase(key)) {
+					JSONArray jsonarr = finalJSON.getJSONArray(key);
+					for (int n = 0; n < jsonarr.length(); n++) {
+						JSONObject object = jsonarr.getJSONObject(n);
 
-    private ArrayList<JSONObject> merge(JSONObject jsonObj1, ArrayList<JSONObject> jsonObjList) throws JSONException {
-        ArrayList<JSONObject> arr = new ArrayList<JSONObject>();
+						JSONObject jsonFinal = new JSONObject();
+						jsonFinal.put(key, object);
 
-        for (JSONObject jsonObj2 : jsonObjList) {
-            JSONObject json = new JSONObject();
-            json.append(Constants.TESTSUITE, jsonObj1);
-            json.append(Constants.TESTSUITE, jsonObj2);
-            arr.add(json);
-        }
+						jsonObjects.add(jsonFinal);
+					}
+				}
+			}
+		}
+		commonElements.remove(Constants.TESTCASE);
+		return merge(commonElements, jsonObjects);
+	}
 
-        return arr;
-    }
+	private ArrayList<JSONObject> merge(JSONObject jsonObj1,
+			ArrayList<JSONObject> jsonObjList) throws JSONException {
+		ArrayList<JSONObject> arr = new ArrayList<JSONObject>();
 
-    private boolean validateXMLSchema(String xsdPath, String xmlString){
-        try {
-            SchemaFactory factory = SchemaFactory.newInstance(Constants.W3C_XML_SCHEMA_NS_URI);
-            Schema schema = factory.newSchema(new File(xsdPath));
-            Validator validator = schema.newValidator();
-            validator.validate(new StreamSource(new StringReader(xmlString)));
-        } catch (IOException | SAXException e) {
-            e.printStackTrace();
-            return false;
-        }
-        return true;
-    }
+		for (JSONObject jsonObj2 : jsonObjList) {
+			JSONObject json = new JSONObject();
+			json.append(Constants.TESTSUITE, jsonObj1);
+			json.append(Constants.TESTSUITE, jsonObj2);
+			arr.add(json);
+		}
+
+		return arr;
+	}
+
+	private boolean validateXMLSchema(String xsdPath, String xmlString) {
+		try {
+			SchemaFactory factory = SchemaFactory
+					.newInstance(Constants.W3C_XML_SCHEMA_NS_URI);
+			Schema schema = factory.newSchema(new File(xsdPath));
+			Validator validator = schema.newValidator();
+			validator.validate(new StreamSource(new StringReader(xmlString)));
+		} catch (IOException | SAXException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	private JSONObject customJSONObject(JSONObject json) throws ParseException,
+			JSONException {
+
+		Iterator<String> keys = json.keys();
+		while (keys.hasNext()) {
+			try {
+				String key = keys.next();
+
+				JSONObject originalJSON = json.getJSONObject(key);
+				if (Constants.TESTCASE.equalsIgnoreCase(key.toString())) {
+					JSONArray jsonArray = new JSONArray();
+					Object testCaseObject = json.get(key.toString());
+
+					if (!(testCaseObject instanceof JSONArray)) {
+						jsonArray.put(testCaseObject);
+
+					}
+					json.remove(Constants.TESTCASE);
+					json.put(key, jsonArray);
+				}
+			} catch (JSONException e) {
+
+			}
+
+		}
+		return json;
+	}
 }
