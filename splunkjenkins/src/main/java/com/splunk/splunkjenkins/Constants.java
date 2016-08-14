d47134b3359d308682f42f3a735fab8635cfe240
Ted Xiao <txiao@splunk.com>
2016-08-14 23:11:27 +0800

    rename tag to event_tag

--- splunkjenkins/src/main/java/com/splunk/splunkjenkins/Constants.java
+++ splunkjenkins/src/main/java/com/splunk/splunkjenkins/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String TESTSUITE = "testsuite";
     public static final String METADATA = "metadata";
     public static final String BUILD_ID = "build_url";
-    public static final String TAG = "tag";
+    public static final String TAG = "event_tag";
     public static final String JOB_RESULT = "job_result";
     public static final String JSON_ENDPOINT = "/services/collector/event";
     public static final String RAW_ENDPOINT = "/services/collector/raw";
