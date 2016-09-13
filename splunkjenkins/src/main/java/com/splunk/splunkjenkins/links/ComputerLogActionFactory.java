cc81f898bf376faea27641364da498e50ad8eff1
Ted Xiao <txiao@splunk.com>
2016-09-13 17:34:07 +0800

    add computer link

--- splunkjenkins/src/main/java/com/splunk/splunkjenkins/links/ComputerLogActionFactory.java
+++ splunkjenkins/src/main/java/com/splunk/splunkjenkins/links/ComputerLogActionFactory.java
@@ -1,17 +1,22 @@
 package com.splunk.splunkjenkins.links;
 
+import com.splunk.splunkjenkins.SplunkJenkinsInstallation;
+import hudson.Extension;
 import hudson.model.Action;
 import hudson.model.Computer;
 import hudson.model.TransientComputerActionFactory;
+import jnr.ffi.annotations.Encoding;
 
 import java.util.Collections;
 import java.util.Collection;
 
-//TODO enable this
+//TODO add agent page and update link
+@Extension
 public class ComputerLogActionFactory extends TransientComputerActionFactory {
     @Override
     public Collection<? extends Action> createFor(Computer target) {
-        return Collections.singleton(new LinkSplunkAction("agent", "name=" + target.getName(),
+        return Collections.singleton(new LinkSplunkAction("agent",
+                "form.host=" + SplunkJenkinsInstallation.get().getMetadataHost() + "&form.agent_name=" + target.getName(),
                 "Splunk"));
     }
 }
