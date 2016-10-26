32c8f332cf2a1331a89ee267a6707c8d3c3c60e8
Ted Xiao <txiao@splunk.com>
2016-10-27 00:10:11 +0800

    allow user to resend build reports generated before plugin installment, e.g. def dataParser=new java.text.SimpleDateFormat("yyyy-MMM-dd HH:mm") def endTime=dataParser.parse("2016-OCT-26 13:24").getTime() def archiver=new com.splunk.splunkjenkins.utils.BuildInfoArchiver() archiver.run(0, endTime)

--- splunk-devops/src/main/java/com/splunk/splunkjenkins/utils/BuildInfoArchiver.java
+++ splunk-devops/src/main/java/com/splunk/splunkjenkins/utils/BuildInfoArchiver.java
@@ -1,12 +1,20 @@
 package com.splunk.splunkjenkins.utils;
 
+import com.splunk.splunkjenkins.SplunkJenkinsInstallation;
+import com.splunk.splunkjenkins.TeeConsoleLogFilter;
 import com.splunk.splunkjenkins.listeners.LoggingRunListener;
 import hudson.model.*;
 import hudson.model.listeners.RunListener;
+import hudson.util.IOUtils;
+import hudson.util.NullStream;
 import hudson.util.RunList;
 import jenkins.model.Jenkins;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+
+import static com.splunk.splunkjenkins.model.EventType.CONSOLE_LOG;
 
 public class BuildInfoArchiver {
     /**
@@ -14,23 +22,27 @@ public class BuildInfoArchiver {
      *
      * @param startTime the start time window of build
      * @param endTime   the end time window of build
+     * @return total number of builds whose result or log was resent
      */
-    public void run(long startTime, long endTime) {
+    public int run(long startTime, long endTime) {
         List<TopLevelItem> topItems = Jenkins.getInstance().getItems();
+        int count = 0;
         for (TopLevelItem topLevelItem : topItems) {
-            run(topLevelItem, startTime, endTime);
+            count += run(topLevelItem, startTime, endTime);
         }
+        return count;
     }
 
     /**
      * @param jobName   the job name, e.g. folder/jobname or job/folder/job/jobname
      * @param startTime the start time window of build
      * @param endTime   the end time window of build
+     * @return total number of builds whose result or log was resent
      */
-    public void run(String jobName, long startTime, long endTime) {
+    public int run(String jobName, long startTime, long endTime) {
         String jobPath = jobName.replace("/job/", "");
         Item item = Jenkins.getInstance().getItem(jobPath, (ItemGroup) null);
-        run(item, startTime, endTime);
+        return run(item, startTime, endTime);
     }
 
     /**
@@ -39,16 +51,18 @@ public class BuildInfoArchiver {
      * @param item      Jenkins job item
      * @param startTime the start time window of build
      * @param endTime   the end time window of build
+     * @return total number of builds whose result or log was resent
      */
-    public void run(Item item, long startTime, long endTime) {
+    public int run(Item item, long startTime, long endTime) {
         if (item == null) {
-            return;
+            return 0;
         }
+        int count = 0;
         if (item instanceof ItemGroup) {
             ItemGroup group = (ItemGroup) item;
             for (Object subItem : group.getItems()) {
                 if (subItem instanceof Item) {
-                    run(((Item) subItem).getName(), startTime, endTime);
+                    count = count + run(((Item) subItem).getName(), startTime, endTime);
                 }
             }
         } else if (item instanceof Project) {
@@ -57,9 +71,25 @@ public class BuildInfoArchiver {
             LoggingRunListener runListener = RunListener.all().get(LoggingRunListener.class);
             for (Run run : runList) {
                 if (run.getStartTimeInMillis() > startTime && run.getStartTimeInMillis() + run.getDuration() < endTime) {
+                    count++;
+                    //resend build event
                     runListener.onCompleted(run, TaskListener.NULL);
+                    if (SplunkJenkinsInstallation.get().isEventDisabled(CONSOLE_LOG)) {
+                        continue;
+                    }
+                    //resend console logs, but with current timestamp
+                    try (InputStream input = run.getLogInputStream()) {
+                        TeeConsoleLogFilter.TeeOutputStream outputStream =
+                                new TeeConsoleLogFilter.TeeOutputStream(new NullStream(), true, run.getUrl() + "console");
+                        IOUtils.copy(input, outputStream);
+                        outputStream.flush();
+                        outputStream.close();
+                    } catch (IOException e) {
+                        //just ignore
+                    }
                 }
             }
         }
+        return count;
     }
 }
