ec04c50077eaf3f9d18b0b5d6a8103d2ecee0e16
Ted Xiao <txiao@splunk.com>
2016-11-28 23:14:40 +0800

    fix test

--- splunk-devops-extend/src/test/java/com/splunk/splunkjenkins/SplunkConsoleLogStepTest.java
+++ splunk-devops-extend/src/test/java/com/splunk/splunkjenkins/SplunkConsoleLogStepTest.java
@@ -10,15 +10,26 @@ import org.junit.Test;
 import org.jvnet.hudson.test.BuildWatcher;
 import org.jvnet.hudson.test.JenkinsRule;
 
+import java.util.UUID;
+
 import static com.splunk.splunkjenkins.SplunkConfigUtil.checkTokenAvailable;
+import static com.splunk.splunkjenkins.SplunkConfigUtil.verifySplunkSearchResult;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.Assert.*;
-
-public class SplunkMessageStepTest {
+public class SplunkConsoleLogStepTest {
     @ClassRule
     public static BuildWatcher buildWatcher = new BuildWatcher();
     @Rule
     public JenkinsRule r = new JenkinsRule();
+    String id = UUID.randomUUID().toString();
+
+    private String jobScript = "sendSplunkConsoleLog {" +
+            "node{\n" +
+            "  sh \"echo testjob\";\n" +
+            "  sh \"echo " + id + "\";\n" +
+            " }" +
+            "}";
 
     @Before
     public void setUp() throws Exception {
@@ -26,12 +37,15 @@ public class SplunkMessageStepTest {
     }
 
     @Test
-    public void testDslScript() throws Exception {
+    public void testSendFile() throws Exception {
+        long startTime = System.currentTimeMillis();
         WorkflowJob p = r.jenkins.createProject(WorkflowJob.class, "p");
-        p.setDefinition(new CpsFlowDefinition("node { sendSplunkScript globalScriptEnabled:false, scriptText:'println \"hello\"' }"));
+        p.setDefinition(new CpsFlowDefinition(jobScript));
         WorkflowRun b1 = r.assertBuildStatusSuccess(p.scheduleBuild2(0));
         assertFalse(b1.isBuilding());
-        r.assertLogContains("hello\n", b1);
+        r.assertLogContains("testjob", b1);
         assertTrue(b1.getDuration() > 0);
+        //check log
+        verifySplunkSearchResult("source=" + b1.getUrl() + "console " + id, startTime, 1);
     }
 }
