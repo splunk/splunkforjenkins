8e297d7285ec4962eaae1da6b4071498271ba0bb
Ted Xiao <txiao@splunk.com>
2016-12-02 17:27:59 +0800

    clean up

--- splunk-devops/src/test/java/com/splunk/splunkjenkins/TestResultAdapterTest.java
+++ splunk-devops/src/test/java/com/splunk/splunkjenkins/TestResultAdapterTest.java
@@ -1,20 +1,13 @@
 package com.splunk.splunkjenkins;
 
-import com.splunk.splunkjenkins.BaseTest;
 import hudson.model.AbstractBuild;
 import hudson.model.FreeStyleProject;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.recipes.LocalData;
-import org.jvnet.hudson.test.recipes.WithPlugin;
 
 import java.util.concurrent.ExecutionException;
 
-import static com.splunk.splunkjenkins.SplunkConfigUtil.checkTokenAvailable;
 import static com.splunk.splunkjenkins.SplunkConfigUtil.verifySplunkSearchResult;
-import static com.splunk.splunkjenkins.utils.LogEventHelper.getPostJobSample;
 
 public class TestResultAdapterTest extends BaseTest {
     @LocalData
